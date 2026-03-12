0d056876157e4d3058e0a811aa7f05f27d4ae36f
AcelyonYT <acelyonyt@gmail.com>
2026-03-12 11:24:17 -0400

    added the engine for pug
    added the engine for pug

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path from "path"
+import { fileURLToPath } from 'url'
 
 import indexAPI from "./routes/indexRoute.js"
+
+// file constants
+export const __filename = fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
 
 const app = express()
 
@@ -10,6 +16,9 @@ app.use(cors)
 app.use(cookieParser)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, "views"))
 
 app.use("/api/index", indexAPI)
 
