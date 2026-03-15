2c42196555a3025e504a71c1eb0b9c13dd6938fa
bcrouthlhu <bcrouthlhu@gmail.com>
2026-03-15 10:11:47 -0400

    Updated Account Creating UI

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(morgan("combined"))
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, "./views"))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/", indexAPI)
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,7 +3,7 @@ import { Router } from "express"
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.render("main.pug")
+    res.render("accountCreate.pug")
 })
 
 export default router
