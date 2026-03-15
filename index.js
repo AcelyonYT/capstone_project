5f1867461f25f50eecadb45084e2039c2df70931
bcrouthlhu <bcrouthlhu@gmail.com>
2026-03-15 11:17:41 -0400

    Added user dashboard

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(morgan("combined"))
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, "./views"))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'css')))
 
 app.use("/", indexAPI)
 
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -14,4 +14,16 @@ router.get("/signup", (req, res) => {
     res.render("accountCreate.pug")
 })
 
+router.get("/dashboard", (req, res) => {
+    res.render("userDashboard.pug")
+})
+
+router.get("/logout", (req, res) => {
+    res.render("login.pug")
+})
+
+router.get("/deleteAccount", (req, res) => {
+    res.render("login.pug")
+})
+
 export default router
